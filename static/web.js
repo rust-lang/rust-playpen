31f9bb01e0fd19b128eae69ec89f15f543dae635
Daniel Micay <danielmicay@gmail.com>
2013-08-14 23:40:37 -0400

    initial commit

---
+++ static/web.js
@@ -0,0 +1,78 @@
+"use strict";
+
+var samples = 2;
+
+function send(path, data, callback) {
+    var request = new XMLHttpRequest();
+    request.open("POST", path, true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.responseType = "json";
+    request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+            callback(request.status, request.response);
+        }
+    }
+    request.send(JSON.stringify(data));
+}
+
+function evaluate(result, code, version) {
+    send("/evaluate.json", {code: code, version: version}, function(rc, object) {
+        if (rc == 200) {
+            result.textContent = object["result"];
+        } else {
+            result.textContent = "connection failure";
+        }
+    });
+}
+
+function format(result, session, version) {
+    send("/format.json", {code: session.getValue(), version: version}, function(rc, object) {
+        if (rc == 200) {
+            if ("error" in object) {
+                result.textContent = object["error"];
+            } else {
+                result.textContent = "";
+                session.setValue(object["result"]);
+            }
+        } else {
+            result.textContent = "connection failure";
+        }
+    });
+}
+
+function sample(session, result) {
+    var request = new XMLHttpRequest();
+    var index = Math.floor(Math.random() * samples);
+    console.log(index);
+    request.open("GET", "/sample/" + index + ".rs", true);
+    request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+            if (request.status == 200) {
+                session.setValue(request.responseText.slice(0, -1));
+            } else {
+                result.textContent = "connection failure";
+            }
+        }
+    }
+    request.send();
+}
+
+addEventListener("DOMContentLoaded", function() {
+    var evaluate_button = document.getElementById("evaluate");
+    var format_button = document.getElementById("format");
+    var result = document.getElementById("result");
+    var version = document.getElementById("version");
+    var editor = ace.edit("editor");
+    var session = editor.getSession();
+
+    session.setMode("ace/mode/rust");
+    sample(session, result);
+
+    evaluate_button.onclick = function() {
+        evaluate(result, session.getValue(), version.options[version.selectedIndex].text);
+    };
+
+    format_button.onclick = function() {
+        format(result, session, version.options[version.selectedIndex].text);
+    };
+}, false);
---
+++ web.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import subprocess
+from bottle import get, post, request, run, static_file
+
+@get("/")
+def serve_index():
+    return static_file("web.html", root="static")
+
+@get("/<path:path>")
+def serve_static(path):
+    return static_file(path, root="static")
+
+@post("/evaluate.json")
+def evaluate():
+    version = request.json["version"]
+    if version not in ("master", "0.7"):
+        return {"error": "invalid version"}
+    print(request.json)
+    with subprocess.Popen(["playpen",
+                           "--user=rust",
+                           "--timeout=5",
+                           "--syscalls-file=whitelist",
+                           "--devices=c:1:9",
+                           "--memory-limit=128M",
+                           "--root=" + version,
+                           "--",
+                           "/usr/local/bin/evaluate.sh",
+                           request.json["code"]],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT) as p:
+        return {"result": p.stdout.read().decode()}
+
+@post("/format.json")
+def format():
+    version = request.json["version"]
+    if version not in ("master", "0.7"):
+        return {"error": "invalid version"}
+    print(request.json)
+    with subprocess.Popen(["playpen",
+                           "--user=rust",
+                           "--timeout=5",
+                           "--syscalls-file=whitelist",
+                           "--devices=c:1:9",
+                           "--memory-limit=128M",
+                           "--root=" + version,
+                           "--",
+                           "/usr/local/bin/format.sh",
+                           request.json["code"]],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT) as p:
+        output = p.communicate()[0][:-1].decode()
+        if p.returncode:
+            return {"error": output}
+        else:
+            return {"result": output}
+
+run(host='0.0.0.0', port=8000, server='cherrypy')
