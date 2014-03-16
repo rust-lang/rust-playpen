7840cdff20ba476b0c36734db2071832ca68c216
Daniel Micay <danielmicay@gmail.com>
2014-03-16 00:22:49 -0400

    update for rust master

--- bin/irc.sh
+++ bin/irc.sh
@@ -5,12 +5,18 @@
 set -o errexit
 
 rustc - -o out <<EOF
-#[feature(globs, macro_rules, struct_variant)];
+#[feature(globs, macro_rules, struct_variant, simd, asm)];
 
-extern crate extra;
+extern crate collections;
+extern crate native;
 
 #[allow(dead_code)]
 static version: &'static str = "$(rustc -v | tail | head -1)";
+
+#[start]
+fn start(argc: int, argv: **u8) -> int {
+    native::start(argc, argv, main)
+}
 
 fn main() {
     let r = {
