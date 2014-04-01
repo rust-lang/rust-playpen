1350bd87e0d2b7d0fa75480388b93d1e5738fa74
Daniel Micay <danielmicay@gmail.com>
2014-04-01 09:46:51 -0400

    no point in creating an unused /dev/null

--- init.sh
+++ init.sh
@@ -19,7 +19,6 @@ pacstrap -c -d root-master \
 
 mkdir root-master/dev/shm
 mknod -m 644 root-master/dev/urandom c 1 9
-mknod -m 666 root-master/dev/null c 1 3
 arch-chroot root-master useradd -m -g users -s /bin/bash rust
 install -m755 bin/* root-master/usr/local/bin
 
