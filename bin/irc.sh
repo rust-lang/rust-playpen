838de619905fddf56f2eac49a86ecc4bc28a4277
Daniel Micay <danielmicay@gmail.com>
2014-04-24 20:57:55 -0400

    tweak the irc template

--- bin/irc.sh
+++ bin/irc.sh
@@ -5,13 +5,16 @@
 set -o errexit
 
 rustc - -o out <<EOF
-#![feature(globs, macro_rules, struct_variant, simd, asm)]
+#![feature(asm, globs, macro_rules, struct_variant, simd, quad_precision_float)]
 
 extern crate collections;
 extern crate native;
 
 #[allow(dead_code)]
 static version: &'static str = "$(rustc -v | tail | head -1)";
+
+#[allow(dead_code)]
+fn show<T: std::fmt::Show>(e: T) { println!("{}", e) }
 
 fn main() {
     let r = {
