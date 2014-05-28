084aa66aba776f3de6745b2a62ad6f891f096ab9
Daniel Micay <danielmicay@gmail.com>
2014-05-28 19:34:28 -0400

    make the initialization script a bit more clever

--- init.sh
+++ init.sh
@@ -2,10 +2,9 @@
 
 umask 022
 
-rm -rf root-master
-mkdir root-master
+mkdir root-master.new
 
-pacstrap -c -d root-master \
+pacstrap -c -d root-master.new \
     bash \
     coreutils \
     grep \
@@ -17,11 +16,19 @@ pacstrap -c -d root-master \
     shadow \
     util-linux
 
-mkdir root-master/dev/shm
-mknod -m 644 root-master/dev/urandom c 1 9
-arch-chroot root-master useradd -m -g users -s /bin/bash rust
-install -m755 bin/* root-master/usr/local/bin
+mkdir root-master.new/dev/shm
+mknod -m 644 root-master.new/dev/urandom c 1 9
+arch-chroot root-master.new useradd -m -g users -s /bin/bash rust
+install -m755 bin/* root-master.new/usr/local/bin
 
-cp -a root-master root-0.10
-pacman -r root-master -S rust-git --noconfirm
-pacman -r root-0.10 -S rust --noconfirm
+cp -a root-master.new root-0.10.new
+pacman -r root-master.new -S rust-git --noconfirm
+pacman -r root-0.10.new -S rust --noconfirm
+
+[[ -d root-master ]] && mv root-master root-master.old
+mv root-master.new root-master
+[[ -d root-master.old ]] && rm -rf root-master.old
+
+[[ -d root-0.10 ]] && mv root-0.10 root-0.10.old
+mv root-0.10.new root-0.10
+[[ -d root-0.10.old ]] && rm -rf root-0.10.old
