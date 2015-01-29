1fbea75b033985d8436247936c1724faac945752
Alex Crichton <alex@alexcrichton.com>
2015-01-28 18:40:48 -0800

    Install rustc from rustup

--- init.sh
+++ init.sh
@@ -27,15 +27,18 @@ mknod -m 644 root-master.new/dev/urandom c 1 9
 arch-chroot root-master.new useradd -m rust
 install -m755 bin/* root-master.new/usr/local/bin
 
-rm -rf root-0.12.0.new
-cp -a root-master.new root-0.12.0.new
-pacman -r root-master.new -S rust-git --noconfirm
-pacman -r root-0.12.0.new -S rust --noconfirm
+#rm -rf root-0.12.0.new
+#cp -a root-master.new root-0.12.0.new
+#pacman -r root-master.new -S rust-git --noconfirm
+#pacman -r root-0.12.0.new -S rust --noconfirm
+
+curl -O https://static.rust-lang.org/rustup.sh
+sh rustup.sh --prefix=root-master.new
 
 [[ -d root-master ]] && mv root-master root-master.old
 mv root-master.new root-master
 [[ -d root-master.old ]] && rm -rf root-master.old
 
-[[ -d root-0.12.0 ]] && mv root-0.12.0 root-0.12.0.old
-mv root-0.12.0.new root-0.12.0
-[[ -d root-0.12.0.old ]] && rm -rf root-0.12.0.old
+#[[ -d root-0.12.0 ]] && mv root-0.12.0 root-0.12.0.old
+#mv root-0.12.0.new root-0.12.0
+#[[ -d root-0.12.0.old ]] && rm -rf root-0.12.0.old
