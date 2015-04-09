7b4303e30b17c99fbd19bb3c21e6533b8a34826b
Alex Crichton <alex@alexcrichton.com>
2015-04-08 17:03:03 -0700

    Default to beta instead of master

--- web.py
+++ web.py
@@ -59,7 +59,7 @@ def extractor(key, default, valid):
 
 @route("/evaluate.json", method=["POST", "OPTIONS"])
 @enable_post_cors
-@extractor("version", "master", ("master",))
+@extractor("version", "beta", ("beta",))
 @extractor("optimize", "2", ("0", "1", "2", "3"))
 def evaluate(optimize, version):
     out, _ = execute(version, "/usr/local/bin/evaluate.sh", (optimize,), request.json["code"])
@@ -77,7 +77,7 @@ def evaluate(optimize, version):
 
 @route("/format.json", method=["POST", "OPTIONS"])
 @enable_post_cors
-@extractor("version", "master", ("master",))
+@extractor("version", "beta", ("beta",))
 def format(version):
     out, rc = execute(version, "/usr/local/bin/format.sh", (), request.json["code"])
     split = out.split(b"\xff", 1)
@@ -88,7 +88,7 @@ def format(version):
 
 @route("/compile.json", method=["POST", "OPTIONS"])
 @enable_post_cors
-@extractor("version", "master", ("master",))
+@extractor("version", "beta", ("beta",))
 @extractor("optimize", "2", ("0", "1", "2", "3"))
 @extractor("emit", "asm", ("asm", "llvm-ir"))
 def compile(emit, optimize, version):
