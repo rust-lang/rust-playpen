1c503334bdecfea5b857b64059b863b3c37d2d7a
Alex Crichton <alex@alexcrichton.com>
2015-06-12 10:30:20 -0700

    Tidy up a comment a bit

--- bin/evaluate.sh
+++ bin/evaluate.sh
@@ -5,9 +5,9 @@ set -o errexit
 TERM=xterm rustc - -o ./out "$@"
 printf '\377' # 255 in octal
 if [ "${*#*--test}" != "$*" ] && [ "${*#*--color=always}" != "$*" ]; then
-	# For /evaluate.json, we have {test: true, color: true}. Let's make the output coloured too.
-	# This would be better in web.py, but we don't have an easy way to alot parameters for ./out.
-	# Really, these shell scripts shouldn't exist at all.
+        # For /evaluate.json, we have {test: true, color: true}. Let's make the
+        # output coloured too.  This would be better in web.py, but we don't
+        # have an easy way to allot parameters for ./out.
 	TERM=xterm exec ./out --color=always
 else
 	exec ./out
